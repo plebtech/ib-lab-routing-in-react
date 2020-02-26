6ff390327ffec80b74c0a0c6f4ad737ac892fb67
Nick Mc <nbmc@protonmail.com>
2020-02-26 10:36:29 -0600

    basic structuring. installed isomorphic-fetch package.

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+
+class App extends Component {
+    render() {
+        return (
+            <div id="app-container">
+                <h1>returned.</h1>
+            </div>
+        );
+    }
+}
+
+export default App;
