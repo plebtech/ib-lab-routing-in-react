6036ea00de0700d3eb828bade117d3c9fe14cbcd
Nick Mc <nbmc@protonmail.com>
2020-02-26 10:45:45 -0600

    installed react-router-dom package. basic formatting.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import 'isomorphic-fetch';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
     render() {
