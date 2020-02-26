911c79cd3d766b413a187cbe2fdfa55031f6f44f
Nick Mc <nbmc@protonmail.com>
2020-02-26 15:03:43 -0600

    more styling tweaks.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,7 +10,8 @@ class Home extends Component {
                 <h1>welcome.</h1>
                 <h2>routing the studio ghibli api.</h2>
                 <h3>this is a demonstration of routing in react, implementing RESTful api calls to the studio ghibli film catalog.</h3>
-                <h3>use the buttons above to view various sorted data. 'films' returns the studio's body of work, 'people' returns a manifest of characters, and so on.</h3>
+                <h3>use the buttons above to view various sorted data. 'films' returns the studio's body of work, 'people' returns a manifest of characters, and so on. from there you can 'learn more' to focus on a single film/character.</h3>
+                <hr />
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, possimus.</p>
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto at expedita cum ipsum perspiciatis aperiam voluptatem, provident ipsa laudantium nostrum?</p>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime, obcaecati dignissimos et consequuntur culpa nisi earum, nihil quam perferendis praesentium, ea voluptates sapiente. Necessitatibus, maiores possimus voluptas quaerat quae quas.</p>
